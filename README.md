472f776a2dbbb9857392a2e78f5108076d9412c0
Jaeyun Noh <jaeyun@gmail.com>
2023-08-27 22:02:47 -0700

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# llcui small llama.ccp wrapper for Node.js
+
+**Note: This project is a work in progress.**
+
+This project provides a Node.js server for a chat user interface (UI) that interacts with the Llama.cpp library. It allows users to communicate with the Llama.cpp application via a web-based chat interface.
+
+## Installation
+
+1. Clone the repository:
+
+git clone https://github.com/ggerganov/llama.cpp.git 
+
+2. Build Lllama.cpp with GPU support
+
+cd llama.cpp
+
+sed -i 's/-arch=native/-arch=all/g' Makefile
+
+make clean && LLAMA_CUBLAS=1 make -j
+
+3. Clone fullmetal-uncensored-agent-demo
+   
+git clone https://github.com/shaileshminsnapsys/fullmetal-uncensored-agent-demo
+
+4. Install the project and set your configuration parameters
+  
+cd fullmetal-uncensored-agent-demo
+
+Download the file from https://huggingface.co/TheBloke/Wizard-Vicuna-7B-Uncensored-GGML/resolve/main/Wizard-Vicuna-7B-Uncensored.ggmlv3.q5_1.bin and save it in root folder
+
+npm install
+
+Open config.js and change path to llama.cpp main file, and the model name/path
+
+## Usage
+To run just type:
+
+npm start
---
+++ server.js
@@ -0,0 +1,51 @@
+//Installation
+// git clone https://github.com/ggerganov/llama.cpp.git
+// cd llama.cpp
+// sed -i 's/export NVCCFLAGS="-arch=native"/export NVCCFLAGS="-arch=all"/' llama.cpp/Makefile
+// make clean && LLAMA_CUBLAS=1 make  -j
+//Copyright Denis Spasyuk
+//License MIT
+
+const { spawn } = require('child_process');
+const config = require('./config.js');
+const Fullmetal = require('fullmetal-agent');
+let tokenLength = 0;
+const fullMetalConfig = {
+  headers: {
+    lucidApiKey: 'sk-lucid-api-key',
+  },
+  name: 'Uncensored Agent',
+};
+const fullmetalAgent = new Fullmetal(fullMetalConfig);
+fullmetalAgent.setApiKey('sample-key');
+fullmetalAgent.onPrompt(async (prompt) => {
+  await getApiResponse(prompt, async (answer, completed) => {
+    fullmetalAgent.sendResponse(answer, completed);
+  });
+});
+
+const getApiResponse = async (input, cb) => {
+  const startTime = Date.now();
+  tokenLength = 0;
+  const response = spawn(config.llamacpp, [...config.params, '-p', `${input}`]);
+
+  response.stdout.on('data', (msg) => {
+    msg = msg.toString('utf-8');
+    if (msg && !msg.includes(input)) {
+      tokenLength += msg.length;
+      cb(msg, false);
+    }
+  });
+
+  response.on('exit', function () {
+    cb(null, true);
+    const endTime = Date.now();
+    // Calculate the elapsed time in seconds
+    const elapsedTimeInSeconds = (endTime - startTime) / 1000;
+
+    const tokensPerSecond = tokenLength / elapsedTimeInSeconds;
+    console.log(`nGPU: ${config.params[3]}`);
+    console.log(`Total time taken: ${elapsedTimeInSeconds}`);
+    console.log(`Tokens Per Second: ${tokensPerSecond.toFixed(2)}`);
+  });
+};
