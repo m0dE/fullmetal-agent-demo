216f6104c0a6efc95bf4eb7efe981d94fae5be3c
shaileshminsnapsys <75966330+shaileshminsnapsys@users.noreply.github.com>
2023-10-10 16:06:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,11 @@ const { spawn } = require('child_process');
 const config = require('./config.js');
 const Fullmetal = require('fullmetal-agent');
 let tokenLength = 0;
-/*const fullmetalConfig = {   
-    model: "TheBloke/Llama-2-7B-fp16", // full name provided in hugging face including the creator's name
-    name: "my 30B LLM", // Optional. This name will be registered in api.fullmetal.ai
-    contextLength: 30, // context length in thousands. 30 here is 30k.
-    acceptPublicPrompts: true // answer public prompts and earn mystery gems
-};
-const fullmetalAgent = new Fullmetal(fullmetalConfig);
-fullmetalAgent.setApiKey('sample-key');
-
-fullmetalAgent.onPrompt(async (prompt) => {
-console.log(prompt);
-  await getApiResponse(prompt, async (response, completed) => {
-    fullmetalAgent.sendResponse(response, completed);
-  });
-});*/
-
-
 
 const modelList = [
   {
     name: 'TheBloke/Wizard-Vicuna-7B-Uncensored.ggmlv3.q5_1',
     file: 'Wizard-Vicuna-7B-Uncensored.ggmlv3.q5_1.bin',
-  },
-  {
-    name: 'TheBloke/Wizard-Vicuna-13B-Uncensored.ggmlv3.q5_1',
-    file: 'Wizard-Vicuna-13B-Uncensored.ggmlv3.q5_1.bin',
   },
 ];
 
@@ -56,18 +35,17 @@ const findMatchingIndexes = (query, models) => {
   return matchingIndexes;
 };
 
-
 const getApiResponse = async (data, cb) => {
   const startTime = Date.now();
   tokenLength = 0;
-   let modelFileUsed, modelNameUsed; 
-  if(data && data.options && data.options.model) {
+  let modelFileUsed, modelNameUsed;
+  if (data && data.options && data.options.model) {
     const findModel = findMatchingIndexes(
-            data.options.model,
-            modelList.map((m) => m.name)
-          );
-          
-    if(!findModel.length) {
+      data.options.model,
+      modelList.map((m) => m.name)
+    );
+
+    if (!findModel.length) {
       findModel = [modelList[0].name];
     }
     modelFileUsed = modelList.filter((m) => m.name === findModel[0])[0].file;
@@ -76,9 +54,9 @@ const getApiResponse = async (data, cb) => {
     modelFileUsed = modelList[0].file;
     modelNameUsed = modelList[0].name;
   }
-  
+
   config.params[1] = modelFileUsed;
-  
+
   const response = spawn(config.llamacpp, [
     ...config.params,
     '-p',
